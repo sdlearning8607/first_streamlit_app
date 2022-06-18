ebe1d7d2d1db9516d51b6817c5b1305ed2ab36c2
Shruti Doshi <107763724+sdlearning8607@users.noreply.github.com>
2022-06-18 12:46:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # let's create checklist to allow customers to multiselect fruits to create their own smoothy
-streamlit.multiselect("Pick some fruits:". list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the fruit table on page
 streamlit.dataframe(my_fruit_list)
