8661bf4d0cafe6cd338ec7122c59d80c4555b802
Shruti Doshi <107763724+sdlearning8607@users.noreply.github.com>
2022-06-18 12:33:53 -0400

    Update streamlit_app.py
    Made changes to Breakfast Menu, adding emojis and two new options to the menu list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
