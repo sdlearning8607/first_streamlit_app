42554fb45d51bda7a08c377d89f6e7900b5d18c7
Shruti Doshi <107763724+sdlearning8607@users.noreply.github.com>
2022-06-19 12:51:36 -0400

    Update streamlit_app.py
    Added code to get User's request for getting fruit related information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit you would like information about?', 'Kiwi')
+streamlit.write('The User entered', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
