5b5f9ec5e4d9d0aa9d4d8e641b2f84bc24370e7e
Shruti Doshi <107763724+sdlearning8607@users.noreply.github.com>
2022-06-19 20:50:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ try:
   else:
     back_from_function = get_fruitvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-except URLerror as e:
+except URLError as e:
   streamlit.error()
 
 #query our trail account data
